a10ae39c9fd0c09c588e1ef5a5a7f82b9bdb3bab
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-23 22:42:20 +0900

    Update 10807.cpp

--- BaaaaaaaaaaaaaaaaaaaaarkingDog/0x03/my_solutions/10807.cpp
+++ BaaaaaaaaaaaaaaaaaaaaarkingDog/0x03/my_solutions/10807.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int num[202];
-
+int arr[202];
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	int n;
-	cin >> n;
-
-	for (int i = 0; i < n; i++)
-	{
-		int input;
-		cin >> input;
-		num[input + 100]++;
-	}
-
-	int findNum;
-	cin >> findNum;
-
-	cout << num[findNum + 100] << "\n";
-
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++)
+    {
+        int input;
+        cin >> input;
+        arr[input + 100]++;
+    }
+    
+    int findNumber;
+    cin >> findNumber;
+    
+    cout << arr[findNumber + 100] << "\n";
+    
+    
+    return 0;
 }
