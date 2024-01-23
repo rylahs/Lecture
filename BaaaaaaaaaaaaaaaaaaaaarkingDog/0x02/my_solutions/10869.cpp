e714eb16b757cef6b4b719097706bca486f2deed
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-23 22:29:17 +0900

    Update 10869.cpp

--- BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/10869.cpp
+++ BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/10869.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	int a, b;
-	cin >> a >> b; // 1 <= a , b <= 10000
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << "\n";
+    cout << a - b << "\n";
+    cout << a * b << "\n";
+    cout << a / b << "\n";
+    cout << a % b << "\n";
 
-	cout << a + b << "\n";
-	cout << a - b << "\n";
-	cout << a * b << "\n";
-	cout << a / b << "\n";
-	cout << a % b << "\n";
-
-	return 0;
+    return 0;
 }
