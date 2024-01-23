074ef406423611dd6b5bd7179d677075dc0c8fc7
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-23 22:29:02 +0900

    Update 10171.cpp

--- BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/10171.cpp
+++ BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/10171.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(void)
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-	cout << "\\    /\\" << "\n";
-	cout << " )  ( ')" << "\n";
-	cout << "(  /  )" << "\n";
-	cout << " \\(__)|" << "\n";
+
+	cout << "\\    /\\\n";
+	cout << " )  ( ')\n";
+	cout << "(  /  )\n";
+	cout << " \\(__)|\n";
+
+
 	return 0;
 }
