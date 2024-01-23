91d9ae71617f913dde104af7be5bdefa3be444a3
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-23 22:30:26 +0900

    Update 2753.cpp

--- BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/2753.cpp
+++ BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/2753.cpp
@@ -3,15 +3,18 @@ using namespace std;
 
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	int year;
-	cin >> year;
+    int year;
+    cin >> year;
 
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-		cout << 1 << "\n";
-	else
-		cout << 0 << "\n";
-	return 0;
+    if ((year % 4 == 0 && year % 100 != 0)
+        || (year % 400 == 0))
+        cout << 1 << "\n";
+    else
+        cout << 0 << "\n";
+
+
+    return 0;
 }
