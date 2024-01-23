084bfd94ba8a325d33c2f6e416a8935ca5560d8d
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-23 22:36:32 +0900

    Update 2444.cpp

--- BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/2444.cpp
+++ BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/2444.cpp
@@ -8,28 +8,33 @@ int main(void)
 
 	int n;
 	cin >> n;
-	for (int i = 1; i <= n; i++)
+
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = n - i; j > 0; j--)
+		for (int j = 0; j < n - i - 1; j++)
 		{
 			cout << " ";
 		}
-		for (int j = 1; j <= 2 * i - 1; j++)
+		for (int j = 0; j < 2 * i + 1; j++)
+		{
 			cout << "*";
+		}
+
 		cout << "\n";
 	}
 
-	for (int i = n - 1; i >= 1; i--)
+	for (int i = 0; i < n - 1; i++)
 	{
-		for (int j = n - i; j > 0; j--)
+		for (int j = 0; j < i + 1; j++)
 		{
 			cout << " ";
 		}
-		for (int j = 1; j <= 2 * i - 1; j++)
+		for (int j = 0; j < 2 * (n - i - 1) - 1; j++)
+		{
 			cout << "*";
+		}
 		cout << "\n";
 	}
-
 
 	return 0;
 }
