1a841275047facd637035bb1ef285bb72e1606de
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-23 22:29:39 +0900

    Update 9498.cpp

--- BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/9498.cpp
+++ BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/9498.cpp
@@ -3,18 +3,23 @@ using namespace std;
 
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	int score;
-	cin >> score;
+    int score;
+    cin >> score;
 
-	if (score >= 90 && score <= 100) cout << "A\n";
-	else if (score >= 80 && score < 90) cout << "B\n";
-	else if (score >= 70 && score < 80) cout << "C\n";
-	else if (score >= 60 && score < 70) cout << "D\n";
-	else cout << "F\n";
+    if (score >= 90)
+        cout << "A\n";
+    else if (score >= 80)
+        cout << "B\n";
+    else if (score >= 70)
+        cout << "C\n";
+    else if (score >= 60)
+        cout << "D\n";
+    else
+        cout << "F\n";
 
 
-	return 0;
+    return 0;
 }
