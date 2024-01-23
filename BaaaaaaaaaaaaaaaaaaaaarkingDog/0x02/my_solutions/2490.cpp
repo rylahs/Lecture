9fb3286913b89c24bc1255313c826a368fb0efcd
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-23 22:30:44 +0900

    Update 2490.cpp

--- BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/2490.cpp
+++ BaaaaaaaaaaaaaaaaaaaaarkingDog/0x02/my_solutions/2490.cpp
@@ -3,40 +3,27 @@ using namespace std;
 
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    // µî °¹¼ö 
+    vector<char> yut = { 'D', 'C', 'B', 'A', 'E' };
 
-	int n = 3;
+    for (int t = 0; t < 3; t++)
+    {
+        int oneCnt = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            int input;
+            cin >> input;
+            if (input == 1)
+                oneCnt++;
+        }
 
-	while (n--)
-	{
-		int sum = 0;
-		int input;
-		for (int i = 0; i < 4; i++)
-		{
-			cin >> input;
-			sum += input;
-		}
+        cout << yut[oneCnt] << "\n";
 
-		switch (sum)
-		{
-		case 0:
-			cout << "D\n";
-			break;
-		case 1:
-			cout << "C\n";
-			break;
-		case 2:
-			cout << "B\n";
-			break;
-		case 3:
-			cout << "A\n";
-			break;
-		case 4:
-			cout << "E\n";
-			break;
-		}
-	}
+    }
 
-	return 0;
+
+    return 0;
 }
