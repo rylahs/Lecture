2326749d8583442489fd4d9ce736de2864b98651
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-23 22:39:47 +0900

    Update 2577.cpp

--- BaaaaaaaaaaaaaaaaaaaaarkingDog/0x03/my_solutions/2577.cpp
+++ BaaaaaaaaaaaaaaaaaaaaarkingDog/0x03/my_solutions/2577.cpp
@@ -6,21 +6,22 @@ int main(void)
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
+	vector<int> n(10, 0);
+
 	int a, b, c;
 	cin >> a >> b >> c;
 
 	int res = a * b * c;
 
-	vector<int> num(10, 0);
-
 	while (res > 0)
 	{
-		num[res % 10]++;
+		n[res % 10]++;
+
 
 		res /= 10;
 	}
 
-	for (auto e : num)
+	for (auto& e : n)
 		cout << e << "\n";
 
 	return 0;
